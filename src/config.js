67f56874b7cc7d5c20ef562661608d917a8586cc
Anya Joy <urda-a@mail.ru>
2022-01-04 12:00:25 +0200

    server link update

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // export const URLrequests = process.env.NODE_ENV == 'production' ? "https://ipet-adoption.herokuapp.com" : "http://localhost:3006"
 
-export const URLrequests = "https://ipet-adoption.herokuapp.com";
+export const URLrequests = 'https://ipet-adoption-service.herokuapp.com';
 // export const URLrequests = "http://localhost:3006";
